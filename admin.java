85969b5a868e952616a0d67caca578fa4811e6bc
siucs006 <siucs006@cs-pc49-192>
2019-10-03 16:01:58 -0500

    Added functions to admin and employee also item and login classes

--- admin.java
+++ admin.java
@@ -1,6 +1,23 @@
-public class admin
+public class admin implements user
 {
+	public orderItem(item* item_)
+	{
+		
+	}
 	
+	public void addUser(String name, string pass)
+	{
+		
+		
+	}
 	
+	public void receiveAlerts()
+	{
+		
+	}
 	
+	public addItem(String newItem)
+	{
+		
+	}
 }
--- employee.java
+++ employee.java
@@ -1,6 +1,15 @@
 public class employee
 {
+	private String name;
+	private String password;
 	
+	public addStock(item* item_)
+	{
+		
+	}
 	
-	
+	public subtractStock(item* item_)
+	{
+		
+	}
 }
